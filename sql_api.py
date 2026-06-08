60c19cce3fec435c7ff9d24b928cbb2f4f539ce3
Ashith Sanjay Rao <ashithsanjayrao1@gmail.com>
2026-06-08 22:32:13 +0530

    Added CORS so frontend can talk to backend

--- sql_api.py
+++ sql_api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 import models
 import schemas
@@ -7,6 +8,14 @@ from database import engine ,sessionLocal
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = sessionLocal()
